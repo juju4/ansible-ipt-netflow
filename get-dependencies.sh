0e5ba2b9a27fafec0d526b2c72b5ebf3ba3fdd0d
juju4 <juju4@users.noreply.github.com>
2018-12-21 12:17:41 -0500

    update galaxy naming

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,15 +10,14 @@ rolesdir=$(dirname $0)/..
 fi
 
 [ ! -d $rolesdir/juju4.maxmind ] && git clone https://github.com/juju4/ansible-maxmind $rolesdir/juju4.maxmind
-[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 [ ! -d $rolesdir/kbrebanov.selinux ] && git clone https://github.com/kbrebanov/ansible-selinux.git $rolesdir/kbrebanov.selinux
 [ ! -d $rolesdir/kbrebanov.dkms ] && git clone https://github.com/kbrebanov/ansible-dkms.git $rolesdir/kbrebanov.dkms
 [ ! -d $rolesdir/kbrebanov.kernel_headers ] && git clone https://github.com/kbrebanov/ansible-kernel_headers.git $rolesdir/kbrebanov.kernel_headers
 [ ! -d $rolesdir/kbrebanov.virtualbox ] && git clone https://github.com/kbrebanov/ansible-virtualbox.git $rolesdir/kbrebanov.virtualbox
 ## galaxy naming: kitchen fails to transfer symlink folder
-#[ ! -e $rolesdir/juju4.ipt-netflow ] && ln -s ansible-ipt-netflow $rolesdir/juju4.ipt-netflow
-[ ! -e $rolesdir/juju4.ipt-netflow ] && cp -R $rolesdir/ansible-ipt-netflow $rolesdir/juju4.ipt-netflow
+#[ ! -e $rolesdir/juju4.iptnetflow ] && ln -s ansible-ipt-netflow $rolesdir/juju4.iptnetflow
+[ ! -e $rolesdir/juju4.iptnetflow ] && cp -R $rolesdir/ansible-ipt-netflow $rolesdir/juju4.iptnetflow
 
 ## don't stop build on this script return code
 true
-
